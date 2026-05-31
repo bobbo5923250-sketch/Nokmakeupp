f30243800bca9ee8c05e62a882de7332718d2a74
bobbo5923250-sketch <bobbo5923250@gmail.com>
2026-06-01 01:55:14 +0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import Header from './components/Header';
 import AnimRoutes from './components/AnimRoutes'
 // import router
 import { BrowserRouter as Route } from 'react-router-dom';
-// import motion
-import { motion } from 'framer-motion';
 
 
 const App = () => {
